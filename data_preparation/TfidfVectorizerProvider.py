334ed9fdbbc7d596ff4ae3ab3b32398a49f4a614
Gerolmed <werwolf420@yahoo.de>
2021-01-10 22:04:55 +0100

    Potentially fixed things

--- data_preparation/TfidfVectorizerProvider.py
+++ data_preparation/TfidfVectorizerProvider.py
@@ -24,28 +24,33 @@ class TfidfVectorizerProvider(DataProvider):
 
         # Collect data
         # Collect vocabulary
-        full_data: List[List[int]] = list()
+        raw_data: List[List[str]] = list()
         full_labels: List[str] = list()
 
         for dataKey, dataValue in rawData.items():
             tokens: List[str] = dataValue.get("tokens")
-            raw_transform = td.fit_transform([" ".join(tokens)])
-            transformed_tokens: List[int] = raw_transform.toArray()[0]
             for reviewKey, dataData in dataValue.items():
                 if reviewKey == "tokens":
                     continue
                 # TODO: add option to also consider uncertain sentiments
                 sentiment_indices = [i for i, x in enumerate(dataData["sentiments"]) if x == "whatever"]
                 for index, token in enumerate(tokens):
-                    full_data.append(transformed_tokens)
+                    raw_data.append(tokens)
                     full_labels.append("sentiment" if index in sentiment_indices else "none")
+
         random.seed(seed)
-        random.shuffle(full_data)
+        random.shuffle(raw_data)
 
         random.seed(seed)
         random.shuffle(full_labels)
 
         split_parameter = round(len(full_labels) * 0.7)
 
-        return TfidfVectorizerData(full_data[split_parameter:], full_labels[split_parameter:],
-                                   full_data[:split_parameter], full_labels[:split_parameter])
+        train_data_raw = raw_data[split_parameter:]
+        test_data_raw = raw_data[:split_parameter]
+
+        train_data = td.fit_transform(train_data_raw)
+        test_data = td.transform(test_data_raw)
+
+        return TfidfVectorizerData(train_data, full_labels[split_parameter:],
+                                   test_data, full_labels[:split_parameter])
