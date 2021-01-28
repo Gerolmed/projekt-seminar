cd174b753d19a2c63fb41b2c4d6dd42843207ea9
Gerolmed <werwolf420@yahoo.de>
2021-01-28 17:04:11 +0100

    Fixed key

--- loading/LoadingUtils.py
+++ loading/LoadingUtils.py
@@ -44,8 +44,7 @@ class LoadingUtils:
             for review_key, review_data in value.items():
                 if review_key == "tokens":
                     continue
-                rawData[sentence_key][review_data]["sentiments"] = \
-                    merge_sentiments(review_data.get("sentiments"), review_data.get("sentiments_uncertainty"))
+                rawData[sentence_key][review_key]["sentiments"] = merge_sentiments(review_data.get("sentiments"), review_data.get("sentiments_uncertainty"))
 
 
         keys = list(rawData.keys())
